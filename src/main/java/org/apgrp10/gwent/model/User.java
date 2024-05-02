2986f87c8051efe6acaf79fa4dbe3cf6f6d38d3e
Amirmahdi <gm.ta.tahmasebi@gmail.com>
2024-05-02 20:00:58 +0330

    add setter and getter of (username, email, password, nickname) in User class

--- src/main/java/org/apgrp10/gwent/model/User.java
+++ src/main/java/org/apgrp10/gwent/model/User.java
@@ -1,6 +1,73 @@
 package org.apgrp10.gwent.model;
 
-public class User
-{
+import java.util.*;
 
+public class User {
+    private static final ArrayList<User> allUsers = new ArrayList<>();
+    private String username, password, nickname, email;
+    private static User currentUser = null;
+
+    public User(String username, String password, String nickname, String email) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        allUsers.add(this);
+    }
+
+    //set and get functions:
+    public static boolean haveUserWithUsername(String username) {
+        for (User user : allUsers)
+            if (user.username.equals(username))
+                return true;
+        return false;
+    }
+
+    public static User getUserByName(String username) {
+        for (User user : allUsers)
+            if (user.username.equals(username))
+                return user;
+        return null;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    //end of set and get functions
 }
