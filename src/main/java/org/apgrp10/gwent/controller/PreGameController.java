d49186cb5f24d7d94415832177c695bb072083cc
Amirmahdi <gm.ta.tahmasebi@gmail.com>
2024-06-03 22:23:37 +0330

    adding grade pane to menu :)

--- src/main/java/org/apgrp10/gwent/controller/PreGameController.java
+++ src/main/java/org/apgrp10/gwent/controller/PreGameController.java
@@ -1,7 +1,22 @@
 package org.apgrp10.gwent.controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.event.ActionEvent;
 import javafx.scene.layout.Pane;
+import org.apgrp10.gwent.model.card.CardImage;
+import org.apgrp10.gwent.view.PreGameMenu;
+
+import java.util.ArrayList;
 
 public class PreGameController {
-	//public Pane background;
+	public MFXButton updateButton;
+
+	public void update() {
+		ArrayList<CardImage> arrayList = new ArrayList<>();
+		double number = Math.random() * 100;
+		for(int i = 0; i < number; i++){
+			arrayList.add(new CardImage("neutral_ciri"));
+		}
+		PreGameMenu.currentMenu.updateLists(arrayList);
+	}
 }
--- src/main/java/org/apgrp10/gwent/view/PreGameMenu.java
+++ src/main/java/org/apgrp10/gwent/view/PreGameMenu.java
@@ -1,40 +1,99 @@
 package org.apgrp10.gwent.view;
 
+import io.github.palexdev.materialfx.controls.MFXScrollPane;
 import javafx.application.Application;
+import javafx.beans.binding.IntegerBinding;
 import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import org.apgrp10.gwent.*;
+import org.apgrp10.gwent.model.card.Card;
+import org.apgrp10.gwent.model.card.CardImage;
+
+import java.util.ArrayList;
 
 public class PreGameMenu extends Application {
-	private Stage stage = null;
-	public static void main(String[] args){
+	private Stage stage;
+	private Pane pane;
+	private Scene scene;
+	private GridPane[] lists = new GridPane[4];
+	public static PreGameMenu currentMenu;
+	public static final int screenWidth = 1500, cardWidth = 18;
+	public static final int screenHeight = 800, cardHeight = 5;
+
+	public static void main(String[] args) {
 		launch(args);
 	}
+
 	@Override
 	public void start(Stage stage) throws Exception {
+		currentMenu = this;
 		this.stage = stage;
+		load();
 		setTitle();
-		show();
+		addGradePane();
+
+		stage.show();
 	}
-	private Pane getPane(){
-		Pane pane = R.getFXML("preGame.fxml");
-		pane.setId("preGameMenu");
-		setCursor(pane);
-		return pane;
-	}
-	private void setTitle(){
+
+	private void setTitle() {
 		stage.setTitle("pre game menu");
 		stage.getIcons().add(R.getImage("icons/profile.png"));
 	}
-	private void show(){
-		stage.setScene(new Scene(getPane()));
+
+	private void load() {
+		this.scene = R.getFXML("preGame.fxml");
+		stage.setScene(scene);
+		pane = (Pane) scene.getRoot();
 		stage.setResizable(false);
-		stage.show();
+		stage.setMinWidth(screenWidth);
+		stage.setMaxWidth(screenWidth);
+		stage.setMinHeight(screenHeight);
+		stage.setMaxHeight(screenHeight);
+		setCursor();
 	}
-	private void setCursor(Pane pane){
+
+	private void addGradePane() {
+		for(int i = 0; i < 4; i++) {
+			GridPane gridPane = new GridPane();
+			gridPane.setMinWidth(3 * screenWidth / (double) cardWidth + 0);
+			gridPane.setMaxWidth(gridPane.getMinWidth());
+			gridPane.setMaxHeight(3 * (double) screenHeight / cardHeight + 30);
+			gridPane.setMaxHeight(gridPane.getMinHeight());
+			gridPane.setVgap(5);
+			gridPane.setHgap(5);
+			for (int j = 0; j < 4; j++) {
+				gridPane.getColumnConstraints().add(
+						new ColumnConstraints(screenWidth / (double) cardWidth));
+			}
+			MFXScrollPane scroll = new MFXScrollPane(gridPane);
+			scroll.setLayoutX(50 + 300 * i);
+			scroll.setLayoutY(50);
+			pane.getChildren().add(scroll);
+			lists[i] = gridPane;
+		}
+	}
+
+	public void updateLists(ArrayList<CardImage> arrayList) {
+		int i = 0, j = 0;
+		lists[0].getChildren().clear();
+		for (CardImage cardImage : arrayList) {
+			lists[0].add(new CardImage("monsters_nekker"), i, j);
+			if (i == 2) {
+				i = 0;
+				j++;
+			} else
+				i++;
+		}
+	}
+
+	private void setCursor() {
 		Image cursor = R.getImage("icons/cursor.png");
 		pane.setCursor(new ImageCursor(cursor));
 	}
