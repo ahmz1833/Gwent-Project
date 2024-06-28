a28db25dca7b0624ad115f918ae953625fe0cbaf
AHMZ <ahmz.ut@gmail.com>
2024-06-28 16:27:01 +0330

    Add some scripts for executing server code on server

---
+++ gwent-server/serverrun.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Define variables
+KEY_FILE="$HOME/.ssh/apgrp10_key"
+PASSPHRASE="14032024apgrp10"
+LOCAL_FILE="target/gwent-server-1.0-SNAPSHOT-jar-with-dependencies.jar"
+REMOTE_USER="ubuntu"
+REMOTE_IP="37.152.181.45"
+REMOTE_DIR="/home/ubuntu"
+REMOTE_FILE="$REMOTE_DIR/gwent-server.jar"
+SCREEN_SESSION_NAME="gwent-server"
+
+# Build the project (and package it into a jar file)
+cd .. && mvn clean compile && cd gwent-server && mvn package && \
+
+# First, kill all java processes on the server
+( sshpass -p $PASSPHRASE ssh -i "$KEY_FILE" $REMOTE_USER@$REMOTE_IP "pkill java"; \
+
+# Use rsync to upload the file with progress
+sshpass -p $PASSPHRASE rsync -avz -e "ssh -i $KEY_FILE" --progress $LOCAL_FILE $REMOTE_USER@$REMOTE_IP:$REMOTE_FILE && \
+
+# Run the jar file in a screen session
+sshpass -p $PASSPHRASE ssh -t -i $KEY_FILE $REMOTE_USER@$REMOTE_IP "
+  if screen -list | grep -q \"$SCREEN_SESSION_NAME\"; then
+    screen -S $SCREEN_SESSION_NAME -X quit; # Quit existing session
+  fi
+  echo \"#!/bin/bash\" > $REMOTE_DIR/run.sh
+  echo \"echo Starting the server... Press Ctrl-A + D for detaching the screen\" >> $REMOTE_DIR/run.sh
+  echo \"echo\" >> $REMOTE_DIR/run.sh
+  echo \"echo\" >> $REMOTE_DIR/run.sh
+  echo \"java -jar $REMOTE_FILE\" >> $REMOTE_DIR/run.sh
+  echo \"echo\" >> $REMOTE_DIR/run.sh
+  echo \"echo\" >> $REMOTE_DIR/run.sh
+  echo \"read -p 'Press [Enter] key to exit...'\" >> $REMOTE_DIR/run.sh
+  chmod +x $REMOTE_DIR/run.sh
+  screen -h 10000 -S $SCREEN_SESSION_NAME $REMOTE_DIR/run.sh
+  " )
