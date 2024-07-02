d56379000c5766eccd4e116468d87c6675d4bc55
Parsa <parsaymm@gmail.com>
2024-07-02 12:14:48 +0330

    add toBase64 and fromBase64 for commands

--- gwent-common/src/main/java/org/apgrp10/gwent/model/Command.java
+++ gwent-common/src/main/java/org/apgrp10/gwent/model/Command.java
@@ -1,6 +1,11 @@
 package org.apgrp10.gwent.model;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Base64;
 
 public interface Command {
 	public static record VetoCard(int player, int cardId) implements Command, Serializable {}
@@ -13,4 +18,27 @@ public interface Command {
 	public static record PickResponse(int player, int cardId, String what) implements Command, Serializable {}
 	public static record Cheat(int player, int cheatId) implements Command, Serializable {}
 	public static record Sync() implements Command, Serializable {}
+
+	default public String toBase64() {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ObjectOutputStream ous = new ObjectOutputStream(baos);
+			ous.writeObject(this);
+			return new String(Base64.getEncoder().encode(baos.toByteArray()));
+		} catch (Exception e) {
+			// wtf?
+			System.exit(1);
+			return null;
+		}
+	}
+
+	static public Command fromBase64(String str) {
+		try {
+			ByteArrayInputStream bais = new ByteArrayInputStream(Base64.getDecoder().decode(str.getBytes()));
+			ObjectInputStream ois = new ObjectInputStream(bais);
+			return (Command)ois.readObject();
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
