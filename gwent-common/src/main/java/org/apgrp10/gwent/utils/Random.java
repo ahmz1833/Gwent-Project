d5a7cf82f406c6ebb99b8dbde9d797cb4c22f876
Parsa <parsaymm@gmail.com>
2024-07-03 09:44:41 +0330

    make Random concurrent by using ThreadLocalRandom

--- gwent-common/src/main/java/org/apgrp10/gwent/utils/Random.java
+++ gwent-common/src/main/java/org/apgrp10/gwent/utils/Random.java
@@ -1,29 +1,27 @@
 package org.apgrp10.gwent.utils;
 
-public class Random {
-	private static final java.util.Random random = new java.util.Random(System.currentTimeMillis());
+import java.util.concurrent.ThreadLocalRandom;
 
-	public static java.util.Random get() {
-		return random;
+public class Random {
+	public static ThreadLocalRandom get() {
+		return ThreadLocalRandom.current();
 	}
 
-	public static long nextLong(long lower, long upper) {return random.nextLong(lower, upper);}
+	public static long nextLong(long lower, long upper) {return get().nextLong(lower, upper);}
 
-	public static int nextInt(int lower, int upper) {return random.nextInt(lower, upper);}
+	public static int nextInt(int lower, int upper) {return get().nextInt(lower, upper);}
 
-	public static boolean nextBoolean() {return random.nextBoolean();}
+	public static boolean nextBoolean() {return get().nextBoolean();}
 
-	public static double nextDouble(double min, double max) {return random.nextDouble() * (max - min) + min;}
+	public static double nextDouble(double min, double max) {return get().nextDouble() * (max - min) + min;}
 
-	public static float nextFloat(float min, float max) {return random.nextFloat() * (max - min) + min;}
+	public static float nextFloat(float min, float max) {return get().nextFloat() * (max - min) + min;}
 
 	public static byte[] nextBytes(int length) {
 		byte[] bytes = new byte[length];
-		random.nextBytes(bytes);
+		get().nextBytes(bytes);
 		return bytes;
 	}
 
-	public static void setSeed(long seed) {random.setSeed(seed);}
-
-	public static long nextUid() {return random.nextLong() & 0x7f_ff_ff_ff_ff_ff_ff_ffl;}
+	public static long nextId() {return get().nextLong() & 0x7f_ff_ff_ff_ff_ff_ff_ffl;}
 }
