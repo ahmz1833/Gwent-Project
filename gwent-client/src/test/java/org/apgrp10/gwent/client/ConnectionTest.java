d54b0637d141db9f574efe31d82803171606b39a
AHMZ <ahmz.ut@gmail.com>
2024-07-04 13:16:54 +0330

    add some test classes

---
+++ gwent-client/src/test/java/org/apgrp10/gwent/client/ConnectionTest.java
@@ -0,0 +1,16 @@
+package org.apgrp10.gwent.client;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ConnectionTest {
+	@BeforeAll
+	public static void setUp() {
+
+	}
+
+	@Test
+	public void test() {
+
+	}
+}
---
+++ gwent-server/src/test/java/org/apgrp10/gwent/server/BackendTest.java
@@ -0,0 +1,22 @@
+package org.apgrp10.gwent.server;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BackendTest {
+	@BeforeAll
+	public static void setUp() {
+
+	}
+
+	@BeforeEach
+	public void setUpEach() {
+
+	}
+
+	@Test
+	public void test() {
+
+	}
+}
